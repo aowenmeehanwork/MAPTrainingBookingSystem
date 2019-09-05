1831dbdf44db75821a327574d9ce8c2049b52845
Reece Bennett <reeceb@kainos.com>
2019-09-05 12:23:25 +0100

    Tidy backend files, pull credentials from mysql.env

--- api/index.js
+++ api/index.js
@@ -1,110 +1,31 @@
 const express = require('express');
-const cors = require('cors')
 const bodyParser = require('body-parser');
-const app = express();
+const cors = require('cors');
+
 const db = require('./db.js');
 
-app.use(bodyParser.urlencoded())
+const app = express();
 
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
 app.use(cors());
 
-var authMiddleware = function (req, res, next) {
-    let token = req.query.token
-    if (token === 'abc') {
-        next();
-    } else {
-        res.send("{error: \"Auth failure\"}");
-    }
+function handleError(err, req, res) {
+    if (err.errno === 3819) err.code = "ER_CHECK_CONSTRAINT_VIOLATED";
+    console.error(`${err.errno} (${err.code}) : ${err.sqlMessage}`);
+    res.status(500).send({
+        message: 'Database error. ' + err.sqlMessage
+    });
 }
 
-// app.use(authMiddleware);
-
-app.post('/addemployee', function(req, res) {
-    db.insertEmployee(req.body, function(insertedKey) {
+app.post('/course', function(req, res) {
+    db.insertTrainingCourse(req.body, function(err, insertedKey) {
+        if (err) return handleError(err, req, res);
+        // TODO: Should we return the inserted item here too?
         res.sendStatus(200);
     });
 });
-
-app.post('/addtrainingcourse', function(req, res) {
-    db.insertTrainingCourse(req.body, function(insertedKey) {
-        res.sendStatus(200);
-    });
-});
-
-app.post('/addsalesemployee', function(req, res) {
-    db.insertSalesEmployee(req.body, function(insertedKey) {
-        res.sendStatus(200);
-    });
-});
-
-// app.get('/getemployeesbydepartment', function(req, res) {
-//     db.getEmployeesByDepartment(req.body,function(rows) {
-//         res.send(rows);
-//     })
-// });
-
-app.get('/employees', function(req, res) {
-    db.getEmployees(function(rows) {
-        res.send(rows);
-    })
-});
-
-app.get('/getemployeesbydepartment', function(req, res) {
-    
-    db.getEmployeesByDepartment(req.query.id , function(rows) {
-        console.log("hbi" + JSON.stringify(req.query.id))
-        res.send(rows);        
-    })
-});
-
-app.get('/getalldepartments', function(req, res) {
-    db.getAllDepartments(function(rows) {
-        res.send(rows);
-    })
-});
-
-// function updateCities(citiesReadyFn) {
-//     db.getCitiesInCountry('GBR', function(rows) {
-//         cities = rows;
-//         citiesReadyFn();
-//     });
-// }
-
-// cities = [];
-
-// app.get('/cities', function(req, res) {
-//     updateCities(function() {
-//         res.send(cities);        
-//     })
-// });
-
-// app.get('/getuser', function(req, res) {
-//     db.getUserById(1, function(rows) {
-//         res.send(rows[0]);        
-//     })
-// });
-
-// app.get('/adduser', function(req, res) {
-//     db.insertUser({email: 'some@test.com', password: 'password', name: 'nametest'})
-//     res.send('check');
-// });
-
-// app.post('/addcity', function(req, res) {
-//     db.addCity(req.body, function(insertedKey) {
-//         updateCities(function() {
-//             res.send(cities);
-//         });
-//     });
-// });
-
-// app.post('/signup', (req, res, next) => {
-//     return passport.authenticate('local', (err, object) => {
-//         if (err) throw err;
-
-//         return res.json({err, object}); 
-//     })(req, res, next);
-//   });
 
 app.listen(8002, function() {
     console.log('World API listening in port 8002');
