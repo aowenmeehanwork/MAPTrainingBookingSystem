8cef86aadc18a1f64b9871e23cac3d7e49decd82
Daniel Kiriasidis Christou <danielch@kainos.com>
2019-09-06 15:00:59 +0100

    Add endpoint tests for adding a course

--- api/test/test.js
+++ api/test/test.js
@@ -24,16 +24,52 @@ describe('Adding a course', function() {
         });
     });
 
-    it('should return status 500 with invalid title', function() {
+    it('should return status 500 with invalid too long title', function() {
         request.post({
             uri: API_URL,
             json: {
-                Course_Title: '',
+                Course_Title: 'qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq',
                 Course_Date: '2019-12-25',
                 Course_Location: 'Frankfurt',
                 Course_Description: 'Little description',
                 Course_Aim: 'Learn some things',
                 Course_Trainer_Name: 'Trainer name'
+            }
+        }, (err, res, body) => {
+            if (err) console.log(err);
+            if (body) console.log(body);
+
+            assert.equal(res.statusCode, 500);
+        });
+    });
+
+    it('should return status 500 with invalid date', function() {
+        request.post({
+            uri: API_URL,
+            json: {
+                Course_Title: 'test',
+                Course_Date: '2019-102-25',
+                Course_Location: 'Frankfurt',
+                Course_Description: 'Little description',
+                Course_Aim: 'Learn some things',
+                Course_Trainer_Name: 'Trainer name'
+            }
+        }, (err, res, body) => {
+            if (err) console.log(err);
+            if (body) console.log(body);
+
+            assert.equal(res.statusCode, 500);
+        });
+    });
+
+    it('should return status 500 with invalid json', function() {
+        request.post({
+            uri: API_URL,
+            json: {
+                Coursle: 'ssds',
+                Course_Date: '2019-12-25',
+                Course_Lion: 'Frankfurt',
+                Courescription: 'Lit'
             }
         }, (err, res, body) => {
             if (err) console.log(err);
