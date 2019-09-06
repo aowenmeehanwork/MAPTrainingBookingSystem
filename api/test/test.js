b9e9c815c75ca5e5260dd020e01b39eb1518d73d
Reece Bennett <reeceb@kainos.com>
2019-09-06 09:30:49 +0100

    Add placeholders for mocha tests

---
+++ api/test/test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+
+describe('Adding a course', function() {
+
+    it('should return status 200 with valid inputs');
+    it('should return status 500 with invalid inputs');
+
+});
