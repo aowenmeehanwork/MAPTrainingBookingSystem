2449060c6f42b53f3f4ca3a88cb886268b166cca
Alex Owen-Meehan <alexo@tribore.kainos.com>
2019-09-06 15:28:32 +0100

    removed comments around courses, forced test suite to skip them instead

--- api/test/getCourseTests.js
+++ api/test/getCourseTests.js
@@ -17,27 +17,28 @@ describe('Getting a course', function() {
     });
 
     // TESTS BELOW ARE FOR /course/id endpoint that does not currently exist, may be useful later
+    // Both tests will be skipped, but still show up in the test report
 
-    // it('Should return specific course with 200 return code', function() {
-    //     request.get({
-    //         uri: 'http://localhost:8002/course/1',
-    //     }, (err, res, body) => {
-    //         if (err) console.log(err);
-    //         if (body) console.log(body);
-    //
-    //         assert.equal(res.statusCode, 500);
-    //     });
-    // });
-    //
-    // it('Should return no courses with 404 return code', function() {
-    //     request.get({
-    //         uri: 'http://localhost:8002/course/shjbdfsfasdf',
-    //     }, (err, res, body) => {
-    //         if (err) console.log(err);
-    //         if (body) console.log(body);
-    //
-    //         assert.equal(res.statusCode, 404);
-    //     });
-    // });
+    it.skip('Should return specific course with 200 return code', function() {
+        request.get({
+            uri: 'http://localhost:8002/course/1',
+        }, (err, res, body) => {
+            if (err) console.log(err);
+            if (body) console.log(body);
+
+            assert.equal(res.statusCode, 500);
+        });
+    });
+
+    it.skip('Should return no courses with 404 return code', function() {
+        request.get({
+            uri: 'http://localhost:8002/course/shjbdfsfasdf',
+        }, (err, res, body) => {
+            if (err) console.log(err);
+            if (body) console.log(body);
+
+            assert.equal(res.statusCode, 404);
+        });
+    });
 
 });
